f4024c09fc41e9a45985ee013efabf9dba8c2c5d
RosannaRocha <98558838+RosannaRocha@users.noreply.github.com>
2022-04-10 20:57:27 -0400

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,39 @@ var question1 = document.getElementById("question-1");
 var question2 = document.getElementById("question-2");
 var question3 = document.getElementById("question-3");
 var question4 = document.getElementById("question-4");
+var timerEl = document.getElementById("timer");
 var instructions = document.getElementById("instructions");
-var time = 100;
-var score = 100;
+var time = 5;
 
 startButton.addEventListener('click', startQuiz);
 
 function changeVisibility(element) {
-    if (element.className == "hidden") {
-      element.className = "visible";
-    } else if (element.className == "visible") {
-      element.className = "hidden";
+    if (element.classList.contains("hidden")) {
+      element.classList.remove("hidden");
+      element.classList.add("visible");
     } else {
-        alert("Error in changing visibility");
+      element.classList.remove("visible");
+      element.classList.add("hidden");
     }
 }
 
 function startQuiz() {
-    changeVisibility(question1);
+
     changeVisibility(instructions);
+    changeVisibility(question1);
+    startTimer();
 }
+
+function startTimer() {
+
+    var timerInterval = setInterval(function() {
+        timerEl.textContent = "Timer: " + time;
+        time--;
+        if (time === -1) {
+            clearInterval(timerInterval);
+        } 
+    }, 1000);
+
+
+}
+
